a4cd5e1c9413cc7ed3ccf11af5273fd9a4b93bc7
Blake <45019373+kinnectusAdmin@users.noreply.github.com>
2024-10-17 09:28:06 -0400

    Initial Commit

---
+++ GrowTherapyProject/GrowTherapyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowTherapyProjectApp.swift
+//  GrowTherapyProject
+//
+//  Created by Blake Osonduagwueki on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GrowTherapyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
